5df4604912839fef814bda319e853126aa960674
Elaidzha1940 <eli080293@gmail.com>
2024-08-12 15:01:40 +0300

    TravelMapApp

---
+++ TravelMapApp/ContentView.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: TravelMapApp
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.08.2024
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelMapApp/TravelMapAppApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: TravelMapApp
+//  File: TravelMapAppApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.08.2024
+//
+//
+
+import SwiftUI
+
+@main
+struct TravelMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
