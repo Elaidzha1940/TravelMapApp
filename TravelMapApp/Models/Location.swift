a3d85bac2b8c7034b9d92ec1a4601012abc45563
Elaidzha1940 <eli080293@gmail.com>
2024-08-13 15:12:44 +0300

    Added Location & MVVM

---
+++ TravelMapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//  /*
+//
+//  Project: TravelMapApp
+//  File: Location.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 13.08.2024
+//
+//  */
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+}
