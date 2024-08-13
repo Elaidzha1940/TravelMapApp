bb6969e33d175422d4e2e35cd04e498ef6211702
Elaidzha1940 <eli080293@gmail.com>
2024-08-13 15:24:50 +0300

    Added LocationDataService

---
+++ TravelMapApp/DataServices/LocationDataService.swift
@@ -0,0 +1,25 @@
+//  /*
+//
+//  Project: TravelMapApp
+//  File: LocationDataService.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 13.08.2024
+//
+//  */
+
+import Foundation
+import MapKit
+
+class LocationDataService {
+    
+    static let location: [Location] = [
+    
+        Location(
+            name: "",
+            cityName: "Cam",
+            coordinates: CLLocationCoordinate2D(latitude: 100, longitude: 200),
+            description: "Cam",
+            imageNames: ["", "", ""],
+            link: "")
+    ]
+}
--- TravelMapApp/Models/Location.swift
+++ TravelMapApp/Models/Location.swift
@@ -11,8 +11,10 @@ import Foundation
 import MapKit
 
 struct Location {
-    
     let name: String
     let cityName: String
     let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
 }
