7d4f49371071d941a8d2de5c851e5871175b28e4
Elaidzha1940 <eli080293@gmail.com>
2024-08-17 19:08:39 +0300

    Added LocationsView

--- TravelMapApp/Views/LocationsView.swift
+++ TravelMapApp/Views/LocationsView.swift
@@ -1,15 +1,23 @@
+//  /*
 //
-//  LocationsView.swift
-//  TravelMapApp
+//  Project: TravelMapApp
+//  File: LocationsView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 17.08.2024
 //
-//  Created by Elaidzha Shchukin on 17.08.2024.
-//
+//  */
 
 import SwiftUI
 
+class LocationsViewModel: ObservedObject {
+    
+}
+
 struct LocationsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+        }
     }
 }
 
