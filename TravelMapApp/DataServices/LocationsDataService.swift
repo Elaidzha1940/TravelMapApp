003648202d6873153a00b62b8658f494938a0038
Elaidzha1940 <eli080293@gmail.com>
2024-08-19 20:57:16 +0300

    Update LocationsViewModel / LocationsView

--- TravelMapApp/DataServices/LocationsDataService.swift
+++ TravelMapApp/DataServices/LocationsDataService.swift
@@ -13,14 +13,14 @@ import MapKit
 class LocationsDataService {
     
     static let location: [Location] = [
-    
+        
         Location(
             name: "Angkor Wat",
             cityName: "Cam",
-            coordinates: CLLocationCoordinate2D(latitude: 13.4125, longitude: 103.8670), /*13.4125° N, 103.8670° E*/
-            description: "Cam",
+            coordinates: CLLocationCoordinate2D(latitude: 13.4125, longitude: 103.8670),
+            description: "Angkor Wat (/ˌæŋkɔːr ˈwɒt/; Khmer, - (City/Capital of Temples) is a Hindu-Buddhist temple complex in Cambodia. Located on a site measuring 162.6 hectares (1,626,000 m2; 402 acres) within the ancient Khmer capital city of Angkor, it is considered the largest religious structure in the world by Guinness World Records. Originally constructed in 1150 CE as a Hindu temple dedicated to the deity Vishnu, it was gradually transformed into a Buddhist temple towards the end of the century.",
             imageNames: ["angkor-wat", "", ""],
-            link: ""),
+            link: "https://en.wikipedia.org/wiki/Angkor_Wat"),
         
         Location(
             name: "Angkor Wat",
--- TravelMapApp/Views/LocationsView.swift
+++ TravelMapApp/Views/LocationsView.swift
@@ -12,13 +12,10 @@ import MapKit
 
 struct LocationsView: View {
     @EnvironmentObject private var vm: LocationsViewModel
-    @State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 13.4125, longitude: 103.8670),
-        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
     
     var body: some View {
         ZStack {
-           Map(coordinateRegion: $mapRegion)
+            Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
         }
     }
