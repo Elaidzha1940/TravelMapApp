8ed9183cd84b7e59ac03a43f82774f55803385d1
Elaidzha1940 <eli080293@gmail.com>
2024-08-23 17:44:14 +0300

    Added LocationPreviewView

---
+++ TravelMapApp/Views/LocationPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationPreviewView.swift
+//  TravelMap
+//
+//  Created by Elaidzha Shchukin on 23.08.2024.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationPreviewView()
+}
