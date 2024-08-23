4a88fad0803c9e5a00fb59d01cd634d3614723ad
Elaidzha1940 <eli080293@gmail.com>
2024-08-23 18:33:16 +0300

    Update LocationPreviewView

--- TravelMapApp/Views/LocationPreviewView.swift
+++ TravelMapApp/Views/LocationPreviewView.swift
@@ -1,18 +1,92 @@
+//  /*
 //
-//  LocationPreviewView.swift
-//  TravelMap
+//  Project: TravelMap
+//  File: LocationPreviewView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 23.08.2024
 //
-//  Created by Elaidzha Shchukin on 23.08.2024.
-//
+//  */
 
 import SwiftUI
 
 struct LocationPreviewView: View {
+    let location: Location
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .bottom, spacing: 0) {
+            VStack(alignment: .leading, spacing: 16) {
+                imageSection
+                titleSection
+            }
+            
+            VStack(spacing: 6) {
+                learnMoreButton
+                nextButton
+            }
+        }
+        .padding(20)
+        .background(
+        RoundedRectangle(cornerRadius: 15)
+            .fill(.ultraThinMaterial))
+        .padding()
     }
 }
 
 #Preview {
-    LocationPreviewView()
+    ZStack {
+        Color.mint.ignoresSafeArea()
+        
+        LocationPreviewView(location: LocationsDataService.location.first!)
+    }
+}
+
+extension LocationPreviewView {
+    private var imageSection: some View {
+        ZStack {
+            if let imageNames = location.imageNames.first {
+                Image(imageNames)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(15)
+            }
+        }
+        .padding(5)
+        .background(Color.white)
+        .cornerRadius(15)
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 3) {
+            Text(location.name)
+                .font(.system(size: 22, weight: .bold, design: .rounded))
+            
+            
+            Text(location.cityName)
+                .font(.system(size: 15, weight: .medium, design: .rounded))
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var learnMoreButton: some View {
+        Button(action: {
+            
+        }, label: {
+            Text("Learn more")
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+                .frame(width: 120, height: 30)
+        })
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var nextButton: some View {
+        Button(action: {
+            
+        }, label: {
+            Text("Next")
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+                .frame(width: 120, height: 30)
+        })
+        .buttonStyle(.bordered)
+    }
 }
