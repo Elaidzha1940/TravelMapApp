d924e4cc75a8bdeca47f37ebd7d610768a77adff
Elaidzha1940 <eli080293@gmail.com>
2024-08-26 18:17:29 +0300

    Update LocationsDataService

--- TravelMapApp/DataServices/LocationsDataService.swift
+++ TravelMapApp/DataServices/LocationsDataService.swift
@@ -19,7 +19,7 @@ class LocationsDataService {
             cityName: "Siem Reap",
             coordinates: CLLocationCoordinate2D(latitude: 13.4125, longitude: 103.8670),
             description: "Angkor Wat (/ˌæŋkɔːr ˈwɒt/; Khmer, - (City/Capital of Temples) is a Hindu-Buddhist temple complex in Cambodia. Located on a site measuring 162.6 hectares (1,626,000 m2; 402 acres) within the ancient Khmer capital city of Angkor, it is considered the largest religious structure in the world by Guinness World Records. Originally constructed in 1150 CE as a Hindu temple dedicated to the deity Vishnu, it was gradually transformed into a Buddhist temple towards the end of the century.",
-            imageNames: ["angkor-wat", "", ""],
+            imageNames: ["angkor-wat", "angkor-wat1", "angkor-wat"],
             link: "https://en.wikipedia.org/wiki/Angkor_Wat"),
         
         Location(
@@ -42,24 +42,26 @@ class LocationsDataService {
             name: "Siem Reap",
             cityName: "Siem Reap",
             coordinates: CLLocationCoordinate2D(latitude: 13.3633, longitude: 103.8564),
-            description: "Cam",
+            description: "Siem Reap (Khmer: សៀមរាប, Siĕm Réab [siəm riəp]) is the second-largest city of Cambodia, as well as the capital and largest city of Siem Reap Province in northwestern Cambodia. Siem Reap has French-colonial and Chinese-style architecture in the Old French Quarter and around the Old Market. In and around the city there are museums, traditional Apsara dance performances, a Cambodian cultural village, souvenir and handicraft shops, silk farms, rice paddies in the countryside, fishing villages and a bird sanctuary near Tonlé Sap, and a cosmopolitan drinking and dining scene. Siem Reap city, home to the famous Angkor Wat temples, was named the ASEAN City of Culture for the period 2021–2022 at the 9th Meeting of the ASEAN Ministers Responsible for Culture and Arts (AMCA) organised on Oct 22, 2020. Today, Siem Reap has many hotels, resorts, and restaurants, due to its proximity to the Angkor Wat temples, Cambodia's most popular tourist attraction.",
             imageNames: ["bayon-temple", "", ""],
-            link: ""),
+            link: "https://en.wikipedia.org/wiki/Siem_Reap"),
         
         Location(
             name: "Bayon Temple",
             cityName: "Siem Reap",
             coordinates: CLLocationCoordinate2D(latitude: 13.4413, longitude: 103.8589),
-            description: "Cam",
+            description: "The Bayon (Prasat Bayoăn [praːsaːt baːjŏən]) (/ˈbaɪɔːn/ BAI-on) is a richly decorated Khmer temple related to Buddhism at Angkor in Cambodia. Built in the late 12th or early 13th century as the state temple of the King Jayavarman VII, the Bayon stands at the centre of Jayavarman's capital, Angkor Thom",
             imageNames: ["siem-reap", "", ""],
-            link: ""),
+            link: "https://en.wikipedia.org/wiki/Bayon"),
         
         Location(
             name: "Ta Prohm",
             cityName: "Siem Reap",
             coordinates: CLLocationCoordinate2D(latitude: 13.4348, longitude: 103.8893),
-            description: "Cam",
+            description: "Ta Prohm (Ta Prôhm, ALA-LC: Tā Brahm [taː prom] - Ancestor Brahma) is the modern name of a temple near the city of Siem Reap, Cambodia, approximately one kilometre east of Angkor Thom and on the southern edge of the East Baray. It was built in the Bayon style largely in the late 12th century and early 13th century and was originally called Rajavihara (Khmer: រាជវិហារ, UNGEGN: Réachvĭhar, ALA-LC: Rājvihār [riəc.vihiə]; - Royal Monastery). It was founded by the Khmer King Jayavarman VII as a Mahayana Buddhist monastery and center of learning dedicated to his mother. Almost 80,000 people were required to live in or visit the temple, including over 2,700 officials and 615 dancers.",
             imageNames: ["ta-prohm", "", ""],
-            link: "")
+            link: "https://en.wikipedia.org/wiki/Ta_Prohm")
     ]
 }
+
+
--- TravelMapApp/Views/LocationDeatilView.swift
+++ TravelMapApp/Views/LocationDeatilView.swift
@@ -10,13 +10,36 @@
 import SwiftUI
 
 struct LocationDeatilView: View {
+    let location: Location
+    
     var body: some View {
-        VStack {
-            
+        ScrollView {
+            VStack {
+                imageSection
+                    .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 10)
+            }
         }
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
-    LocationDeatilView()
+    LocationDeatilView(location: LocationsDataService.location.first!)
+}
+
+extension LocationDeatilView {
+    
+    private var imageSection: some View {
+        TabView {
+            ForEach(location.imageNames, id: \.self) {
+                Image($0)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width)
+                    .clipped()
+            }
+        }
+        .frame(height: 500)
+        .tabViewStyle(PageTabViewStyle())
+    }
 }
